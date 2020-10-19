93108ce35ebaef41fad823040af3db9ef5ca0823
Aaron Ortiz <aronortiz90@yahoo.com>
2020-10-19 09:47:57 -0500

    add numberOfCharacters function and test

--- src/main/java/aaron/ortiz/Name.java
+++ src/main/java/aaron/ortiz/Name.java
@@ -1,5 +1,34 @@
 package aaron.ortiz;
 
 public class Name {
+    private String chainCharacters;
+    private int lenght;
 
+
+    public Name(String chainCharacters, int lenght) {
+        this.chainCharacters = chainCharacters;
+        this.lenght=lenght;
+
+
+    }
+    public String getChainCharacters() {
+        return chainCharacters;
+    }
+
+    public void setChainCharacters(String chainCharacters) {
+        this.chainCharacters = chainCharacters;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public int numberOfCharacters(){
+        return this.lenght=this.chainCharacters.length();
+    }
 }
+
--- src/test/java/aaron/ortiz/NameTest.java
+++ src/test/java/aaron/ortiz/NameTest.java
@@ -1,7 +1,17 @@
 package aaron.ortiz;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NameTest {
+    private Name name;
+    @Test
+    public void numberOfCharacters(){
+        this.name=new Name("Hola",4);
+        assertEquals(4,this.name.numberOfCharacters());
+    }
+
+
 
 }
